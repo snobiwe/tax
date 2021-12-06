01a7ac87991946790dedf53294f9d09aef76edf3
snobiwe <85409668+snobiwe@users.noreply.github.com>
2021-12-06 19:48:35 +0300

    Deploy

--- src/route.js
+++ src/route.js
@@ -1,5 +1,5 @@
 import React, { memo } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Page from './screens/page/page'
 
